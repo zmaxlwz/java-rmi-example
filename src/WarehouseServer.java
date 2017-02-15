d815b14d497ef2cff716fa2ac5b3aef1828f6787
Wenzhao Liu <zmaxlwz@gmail.com>
2017-02-14 17:02:19 -0800

    initial commit
    initial commit

---
+++ src/WarehouseServer.java
@@ -0,0 +1,20 @@
+
+import java.rmi.*;
+import javax.naming.*;
+
+public class WarehouseServer{
+	
+	public static void main(String[] args) throws RemoteException, NamingException{
+
+		System.out.println("Constructing server implementation...");
+		WarehouseImpl centralWarehouse = new WarehouseImpl();
+
+		System.out.println("Binding server implementation to registry...");
+		Context namingContext = new InitialContext();
+		namingContext.bind("rmi:central_warehouse", centralWarehouse);
+
+		System.out.println("Waiting for invocations from clients...");
+		
+	}
+
+}
